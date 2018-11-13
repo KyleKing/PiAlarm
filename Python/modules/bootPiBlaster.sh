55269f2a3cf1b34bd8be7b08eed48d58d8f89dcd
Kyle King <kmking72@gmail.com>
2018-11-12 20:40:14 -0500

    Rm: clear out directory for fresh start

--- Python/modules/bootPiBlaster.sh
+++
@@ -1,6 +0,0 @@
-echo Manually Starting Pi Blaster
-# # The regular list of GPIOs
-# sudo ~/pi-blaster/pi-blaster --gpio 4,17,18,27,21,22,23,24,25
-
-# The custom list for PiAlarm:
-sudo /home/pi/pi-blaster/pi-blaster --gpio 15,24,7,8,25,16,20,21,23,19
--- bundleEntry.js
+++
@@ -1,1 +0,0 @@
-import Alarms from './modules/Alarms.jsx'; // eslint-disable-line
